f30c3b2453b2ed5afb2e7e3ecb5290ab369a585e
digitalneurohub <digitalneurohub@tutamail.com>
2026-06-13 12:19:28 +0200

    Update config.js

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ const MG_CONFIG = {
   SUPABASE_ANON_KEY: 'sb_publishable_wiMyt2rTURQFrdEAX0gtKQ_0VR8hi0A',
 
   // --- Web Push (genera con: npx web-push generate-vapid-keys) ---
-  VAPID_PUBLIC_KEY: 'INCOLLA-QUI-LA-VAPID-PUBLIC-KEY',
+  VAPID_PUBLIC_KEY: 'BMK9Ee4QEuL5JoJWmu-3z9TMB3Z22s8iinzH7DoNtoYMwKDNw5IJadTxDrS-BHSBuxDnvEAUt3LRG0DhjBK3KIw',
 
   // --- Parametri demo ---
   DEFAULT_PATIENT_CODE: 'MG-001', // prefisso del codice paziente generato al primo avvio
